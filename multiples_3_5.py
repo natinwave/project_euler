4a63cd82f71eb8a49f56d90fd0f1cf5bdb6f297b
natinwave <nwalkeh@udel.edu>
2016-01-28 11:06:28 -0500

    Update multiples_3_5.py

--- multiples_3_5.py
+++ multiples_3_5.py
@@ -3,17 +3,12 @@
 #   x     : counter variable that increments from 0 to 1,000.
 #   total : updated every time 'x' is either a multiple of 3 or 5.
 
-x = 0
 total = 0
+divisible_3_or_5 = [i for i in range(3, 1000) if i % 3 == 0 or i % 5 == 0 ]
 
-# this for loop iterates x from 3 to 1,000.
-for x in range(3, 1000):
-    
-    # The if/elif statement pair checks if 3 or 5 divides evenly into the 
-    # current number. If either one does, it is added (only once) to the total.
-    if x % 3 == 0:
-        total += x
-    elif x % 5 == 0:
-        total += x
+# this for loop iterates x through all the numbers that are divisible by either
+# 3 or 5, and adds them to the total.
+for x in divisible_3_or_5:
+    total += x
 
 print total
