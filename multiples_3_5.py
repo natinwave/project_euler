56c3caed82502df05ab577f9d16be38c6bc2bc8b
natinwave <nwalkeh@udel.edu>
2016-01-28 11:44:19 -0500

    Update multiples_3_5.py

--- multiples_3_5.py
+++ multiples_3_5.py
@@ -1,14 +1,12 @@
-# This code prints the sum of every number from 0 to 1,000 which is a multiple
-# of either 3 or 5.
-#   x     : counter variable that increments from 0 to 1,000.
-#   total : updated every time 'x' is either a multiple of 3 or 5.
+def multiples_3_5(upper_limit):
+    # Finds the sum of every number from 0 to an upper_limit 
+    # which is a multiple of either 3 or 5.
+    
+    # This creates a list of all the numbers from 0 to the 
+    # upper_limit that are divisible by 3 or 5.
+    divisible_3_or_5 = [i for i in range(3, upper_limit) if i % 3 == 0 or i % 5 == 0 ]
+    
+    # Returns the sum of all the elements in divisible_3_or_5.
+    return sum(divisible_3_or_5)
 
-total = 0
-divisible_3_or_5 = [i for i in range(3, 1000) if i % 3 == 0 or i % 5 == 0 ]
-
-# this for loop iterates x through all the numbers that are divisible by either
-# 3 or 5, and adds them to the total.
-for x in divisible_3_or_5:
-    total += x
-
-print total
+print multiples_3_5(1000)
